0c8bc08286a3735e40198f6c5353ba857e72ccaf
Andrew Le <z5162356@ad.unsw.edu.au>
2019-11-16 19:45:35 +1100

    First Commit by Andrew
    Added Scoreboard Recycler Adapter, Fragment, required layout files and changed some logic in the QuizActivity

---
+++ app/src/main/java/com/example/GroupAssignment/Adapters/ScoreboardRecyclerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.GroupAssignment.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.GroupAssignment.R;
+
+import java.util.ArrayList;
+
+public class ScoreboardRecyclerAdapter extends RecyclerView.Adapter<ScoreboardRecyclerAdapter.ScoreboardViewHolder> {
+
+    public ArrayList<Integer> scoreArrayList;
+
+    public static class ScoreboardViewHolder extends RecyclerView.ViewHolder {
+        public TextView score;
+        public ScoreboardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            score = itemView.findViewById(
+                    R.id.scoreTextView);
+
+
+        }
+    }
+
+    public ScoreboardRecyclerAdapter(ArrayList<Integer> scoreList) {
+        scoreArrayList = scoreList;
+    }
+
+    @NonNull
+    @Override
+    public ScoreboardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.scoreboardlayout, parent, false);
+        ScoreboardViewHolder scoreboardViewHolder = new ScoreboardViewHolder(view);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ScoreboardViewHolder holder, int position) {
+        holder.score.setText(scoreArrayList.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return scoreArrayList.size();
+    }
+}
+
+
