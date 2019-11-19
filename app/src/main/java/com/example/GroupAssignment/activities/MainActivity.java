cfdf15c1a9be5b41e2e2e340230f1acf58b0682a
AIison Suherly <alison.suherly@gmail.com>
2019-11-19 20:32:15 +1100

    Update MainActivity.java
    changes1

--- app/src/main/java/com/example/GroupAssignment/activities/MainActivity.java
+++ app/src/main/java/com/example/GroupAssignment/activities/MainActivity.java
@@ -23,15 +23,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-  edits-alison
-//    Button startBtn;
+
 
     BottomNavigationView bottomNavigationView;
 
-=======
     Button startBtn;
     Button apiTestButton;
-  master
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
